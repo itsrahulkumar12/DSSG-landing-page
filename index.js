142b2acd8c88978f7e28f8cc7d83a623b842496f
Rahul Kumar <iamrahulkumar.dev@gmail.com>
2023-02-22 22:24:41 +0530

    minor

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 let acc = document.getElementsByClassName("accordion");
+let sumbit_btn1 = document.querySelector("#cb_btn");
+let sumbit_btn2 = document.querySelector("#cb_btn1");
 
 for (let i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function () {
@@ -11,3 +13,11 @@ for (let i = 0; i < acc.length; i++) {
     }
   });
 }
+
+sumbit_btn1.addEventListener("click", (e) => {
+  e.preventDefault();
+});
+
+sumbit_btn2.addEventListener("click", (e) => {
+  e.preventDefault();
+});
